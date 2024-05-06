c49d327778ca70bd81531423c9d3770407954abb
Bunmi Adekunle <bumspin@gmail.com>
2024-05-06 16:42:06 +0100

    Brisheam Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 import Header from './Component/Header'
 import Button from './Component/Button'
 import style from './App.module.css'
